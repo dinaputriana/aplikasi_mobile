2b47a0b8ee7d48ca0152783c5d45b3f8cc5ed31c
dinaputriana <146522295+dinaputriana@users.noreply.github.com>
2023-10-02 08:28:02 +0700

    Add files via upload

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'belajar_ionicc',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
